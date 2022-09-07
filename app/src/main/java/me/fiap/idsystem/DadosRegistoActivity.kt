ce631dc7912e9e1793cd2d8f61efb07ca30d4b2b
Robert Oliveira <86692878+Robert-Oliveira@users.noreply.github.com>
2022-09-07 11:51:46 -0300

    first commit

---
+++ app/src/main/java/me/fiap/idsystem/DadosRegistoActivity.kt
@@ -0,0 +1,11 @@
+package me.fiap.idsystem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DadosRegistoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dados_registo)
+    }
+}
---
+++ app/src/main/java/me/fiap/idsystem/ui/busca/BuscaActivity.kt
@@ -0,0 +1,11 @@
+package me.fiap.idsystem.ui.busca
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BuscaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_busca)
+    }
+}
